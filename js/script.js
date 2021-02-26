c32395673c339a8d2ae749921f26f115532a9bda
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-02-26 18:30:39 +0100

    deleted original JavaScript (saved the comparison in a repl)

--- js/script.js
+++ js/script.js
@@ -1,28 +1,16 @@
 function newItem(){
 
-// vanilla javascript left in comments for my understanding - will delete later
+// vanilla javascript in comments for my understanding saved in a Repl.it
     
 //1. Adding a new item to the list of items: 
-//   let li = document.createElement("li");
    let li = $('<li></li>');
-//   $('#list').append(li);
-    
-//   let inputValue = document.getElementById("input").value;
    let inputValue = $('#input').val();
-    
-//   let text = document.createTextNode(inputValue);
-//   let text = $('inputValue');
-    
-//   li.appendChild(text);
    li.append(inputValue);
 
     
    if (inputValue === '') {
      alert("You must write something!");
    } else {
-//     let list = document.querySelector('#list');
-//     list.appendChild(li);
-//     let list = $('#list'); //not necessary
      $('#list').append(li);  
    }
 
@@ -30,11 +18,9 @@ function newItem(){
     
  //2. Crossing out an item from the list of items:
    function crossOut() {
-// 		li.classList.toggle("strike");
        li.toggleClass('strike');
  	}
 
-// 	li.addEventListener("dblclick",crossOut);
     li.on("dblclick", function crossOut() {
 		li.toggleClass("strike");
 	});
@@ -42,19 +28,13 @@ function newItem(){
     
     
  //3(i). Adding the delete button "X": 
-//   let crossOutButton = document.createElement("crossOutButton");
    let crossOutButton = $('<crossOutButton></crossOutButton>');
-// 	crossOutButton.appendChild(document.createTextNode("X"));
     crossOutButton.append(document.createTextNode('X'));
-// 	li.appendChild(crossOutButton);
     li.append(crossOutButton);
-
-// 	crossOutButton.addEventListener("click", deleteListItem);
     crossOutButton.on('click', deleteListItem);
     
  //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
    function deleteListItem(){
-// 		li.classList.add("delete")
         li.addClass('delete');
  	}
  // 4. Reordering the items: 
